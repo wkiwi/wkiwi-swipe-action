6e4073f9e4e06bf0a28b0439545f476709b3da14
wkiwi <15619986615@163.com>
2019-03-18 17:27:33 +0800

    init

---
+++ readme.md
@@ -0,0 +1,170 @@
+### SwipeAction 滑动操作
+
+通过滑动触发选项的容器，组件名：``wkiwi-swipe-action``
+
+**使用方式：**
+
+在 ``script`` 中引用组件 
+
+```javascript
+import wkiwiSwipeAction from "@/components/wkiwi-swipe-action"
+export default {
+    components: {wkiwiSwipeAction},
+    messages:[{
+        text: '置顶',
+        style: {
+            backgroundColor: '#C7C6CD'
+        }
+    }, {
+        text: '删除',
+        style: {
+            backgroundColor: '#dd524d'
+        }
+    }],
+    messages:[
+      {
+        title:"马云",
+        url:"http://img1.3lian.com/gif/more/11/201212/0d1252b54be4f2d240b6b7fe4ed35054.jpg",
+        message:"什么鬼，我有支付宝",
+        time:"15:15",
+        count:5,
+        stick: false,//是否为置顶状态
+        disabled: true //是否禁止滑动
+
+      },
+      {
+        title:"马化腾",
+        url:"http://img1.3lian.com/gif/more/11/201212/cd1d745ed855bef27f47c8aff0786067.jpg",
+        message:"哈哈哈，我要一统江湖啦",
+        time:"15:15",
+        count:22,
+        stick: true,
+        disabled: false
+
+        },
+      {
+        title:"李彦宏",
+        url:"http://img1.3lian.com/gif/more/11/201212/c011f2b2ab1a10d79fe931a786503d03.jpg",
+        message:"抄袭我的吧，我早都做过了",
+        time:"12:13",
+        count:1,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"雷军",
+        url:"http://img1.3lian.com/gif/more/11/201212/cf9d56d05c0cbc1edbf21cf64ad9c341.jpg",
+        message:"微信给你下线，看你还嘚瑟",
+        time:"12:11",
+        count:0,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"前端开发者",
+        url:"http://img1.3lian.com/gif/more/11/201212/04b335ead07530a6188a27549fad1a68.jpg",
+        message:"我们的春天来啦，哈哈哈",
+        time:"11:35",
+        count:0,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"微信小程序",
+        url:"http://img1.3lian.com/gif/more/11/201212/e10e8faa201671b5a2e8a6146f80d99e.jpg",
+        message:"谁有内测资格啊啊，300万买一个",
+        time:"08:23",
+        count:0,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"小程序",
+        url:"http://img1.3lian.com/gif/more/11/201212/3fab6379b6ef53e7c17a1cc772eed0d6.jpg",
+        message:"这个IDE方便都不要配置了",
+        time:"03:21",
+        count:5,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"微信应用号开发群",
+        url:"http://img1.3lian.com/gif/more/11/201212/8f3762fb4d791410ec970baa2641344d.jpg",
+        message:"开发的局限性太大了",
+        time:"03:08",
+        count:99,
+        stick: false,
+        disabled: false
+         },
+      {
+        title:"后台",
+        url:"http://img1.3lian.com/gif/more/11/201212/411cffabcd6e4474b48f1ecac76e1b3f.jpg",
+        message:"不看好小程序",
+        time:"02:45",
+        count:0,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"闯天下",
+        url:"http://img1.3lian.com/gif/more/11/201212/04378257474004fcdd62f619d724db0c.jpg",
+        message:"微信太强大了",
+        time:"01:09",
+        count:0,
+        stick: false,
+        disabled: false
+        },
+      {
+        title:"小程序",
+        url:"http://img1.3lian.com/gif/more/11/201212/58ec7c89b16c529fc9d0dc440a72e14d.jpg",
+        message:"有快来看直播啦，学习开始啦",
+        time:"00:24",
+        count:2,
+        stick: false,
+        disabled: false
+         }
+    ]
+}
+```
+
+一般用法
+
+```html
+<wkiwi-swipe-action :options="[
+    {
+            text: '置顶',
+            style: {
+                    backgroundColor: '#C7C6CD'
+            }
+    }, {
+            text: '删除',
+            style: {
+                    backgroundColor: '#dd524d'
+            }
+    }
+]">
+    :messagesList="messages"
+</wkiwi-swipe-action>
+```
+
+
+```
+
+**SwipeAction 属性说明：**
+
+|属性名|类型|默认值|是否必填	|说明|
+|:--|:--|:--|:--|:--|
+|stick|Boolean|false|否|是否为置顶状态|
+|disabled|Boolean|false|否|是否禁止滑动|
+
+
+options 参数说明
+
+|参数|类型|是否必填|说明|
+|:--|:--|:--|:--|
+|text|String|是|按钮的文字|
+|style|Object|否|按钮样式{backgroundColor,color,fontSize}，backgroundColor默认为：#C7C6CD，color默认为：#FFFFFF，fontSize默认为：28upx|
+
+
+## 项目效果图：
+<img src="https://raw.githubusercontent.com/wkiwi/wkiwi-swipe-action/master/demo.gif" width="365" height="619"/>
---
+++ wkiwi-swipe-action.vue
@@ -0,0 +1,237 @@
+<template>
+	<view class="message-list">
+		<block v-for="(it,i) of messagesList" :key="i">
+			<view class="uni-swipe-action">
+				<view class="uni-swipe-action__container" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd"
+				 @touchcancel="touchEnd" :style="{'transform':messageIndex == i ? transformX : 'translateX(0px)','-webkit-transform':messageIndex == i ? transformX : 'translateX(0px)'}" :data-index="i" :data-disabled="it.disabled">
+					<view class="uni-swipe-action__content " @click="toMessageDetail(i)">
+						<view class="item" :class="it.stick  ? 'stick' : ''">
+							<view class="item-left">
+								<image :src="it.url" class="image"/>
+							</view>
+							<view class="item-middle">
+								<text class="title">{{it.title}}</text>
+								<text class="message">{{it.message}}</text>
+						  </view>
+						  <view class="item-right">
+								<view class="time">{{it.time}}</view>
+								<view class="mark" v-if="it.count>0">{{it.count}}</view>
+						  </view>
+						</view>
+						<!-- <view  class="line"></view> -->
+					</view>
+					<view class="uni-swipe-action__btn-group" :id="elId">
+						<div v-for="(item,index) in options" :key="index" class="uni-swipe-action--btn" :style="{backgroundColor: item.style && item.style.backgroundColor ? item.style.backgroundColor : '#C7C6CD',color: item.style && item.style.color ? item.style.color : '#FFFFFF',fontSize: item.style && item.style.fontSize ? item.style.fontSize : '28upx'}"
+						 @click="bindClickBtn(item,i)">
+							{{item.text == '置顶' ? '取消置顶' : item.text}}
+						</div>
+					</view>
+				</view>
+			</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'uni-swipe-action',
+		props: {
+			options: Array,
+			messagesList:Array,
+		},
+
+		data() {
+			const elId = `Uni_${Math.ceil(Math.random() * 10e5).toString(36)}`
+			return {
+				elId: elId,
+				transformX: 'translateX(0px)',
+				messageIndex: -1,
+			}
+		},
+		created() {
+			this.direction = ''
+			this.startX = 0
+			this.startY = 0
+			this.btnGroupWidth = 0
+			this.isMoving = false
+		},
+		// #ifdef H5
+		mounted() {
+			this.getSize()
+		},
+		// #endif
+		// #ifndef H5
+		onReady() {
+			this.getSize()
+		},
+		// #endif
+		methods: {
+			toMessageDetail(i){
+				console.log('进入到聊天界面'+i)
+			},
+			getSize() {
+				uni.createSelectorQuery().in(this).select(`#${this.elId}`).boundingClientRect().exec((ret) => {
+					this.btnGroupWidth = ret[0].width;
+				});
+			},
+			bindClickBtn(item, index) {
+				this.messageIndex = -1;
+				console.log(item.text +'message第'+ index+ '项');
+				
+			},
+			touchStart(event) {
+				if(event.currentTarget.dataset.disabled === true){
+					return;
+				}
+				this.startX = event.touches[0].pageX;
+				this.startY = event.touches[0].pageY;
+			},
+			touchMove(event) {
+				if (this.direction === 'Y' || event.currentTarget.dataset.disabled === true) {
+					this.direction = '';
+					return;
+				}
+				var moveY = event.touches[0].pageY - this.startY,
+						moveX = event.touches[0].pageX - this.startX;
+				if (!this.isMoving && Math.abs(moveY) > Math.abs(moveX)) { //纵向滑动
+					this.direction = 'Y';
+					return;
+				}
+				this.direction = moveX > 0 ? 'right' : 'left';
+				this.messageIndex = moveX < 0 ? event.currentTarget.dataset.index : -1;
+				this.isMoving = true;
+			},
+			touchEnd(event) {
+				this.isMoving = false;
+				if (this.direction !== 'right' && this.direction !== 'left') {
+					this.direction = '';
+					return;
+				}
+				if (this.direction == 'right') {
+					this.messageIndex = -1;
+				} 
+				this.endMove(event)
+			},
+			endMove(event) {
+				if (this.direction === 'Y' || event.currentTarget.dataset.disabled === true) {
+					this.direction = '';
+					return;
+				}
+				if (this.messageIndex !== -1) {
+					this.transformX = `translateX(${-this.btnGroupWidth}px)`;
+					this.$emit('opened');
+				} else {
+					this.transformX = 'translateX(0px)';
+					this.$emit('closed');
+				}
+				this.direction = '';
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.uni-swipe-action {
+		width: 100%;
+		overflow: hidden;
+		&__container {
+			background-color: #FFFFFF;
+			width: 200%;
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+			transition: transform 350ms cubic-bezier(.165, .84, .44, 1);
+		}
+		&__content {
+			width: 50%;
+		}
+		&__btn-group {
+			display: flex;
+			flex-direction: row;
+		}
+		&--btn {
+			padding: 0 32upx;
+			color: #FFFFFF;
+			background-color: #C7C6CD;
+			font-size: 28upx;
+			display: inline-flex;
+			text-align: center;
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+	.item {
+	  display: flex;
+	  flex-direction: row;
+	  justify-content: space-between;
+	  padding: 10upx 16upx;
+	  background-color: #fff;
+		border-bottom:1px solid rgb(243,243,243);
+		&.stick{
+			background-color: rgba(243, 236, 221,.5);
+		}
+		.item-left {
+		  text-align: center;
+			.image {
+			  width: 100upx;
+			  height: 100upx;
+			  border-radius: 50upx;
+			  background-color: #eee;
+			}
+		}
+		.item-middle {
+			display: flex;
+		  flex: 1;
+			flex-wrap: wrap;
+			flex-direction: row;
+			justify-content: flex-start;
+		  margin-left: 10upx;
+		  overflow: hidden;
+			.title {
+				width: 100%;
+			  color: #000;
+			  font-family: "微软雅黑";
+			  font-weight: 400;
+			  font-size: 30upx;
+				height: 50upx;
+			  line-height:60upx;
+			  overflow: hidden; /*自动隐藏文字*/
+			  text-overflow: ellipsis; /*文字隐藏后添加省略号*/
+				white-space: nowrap; /**强制不换行*/
+			}
+			.message {
+				width: 100%;
+			  font-family: "微软雅黑";
+			  color: #808080;
+				height: 50upx;
+			  line-height: 40upx;
+			  font-size: 24upx;
+			  overflow: hidden; /**自动隐藏文字*/
+			  text-overflow: ellipsis; /**文字隐藏后添加省略号*/
+			  white-space: nowrap; /**强制不换行*/
+			}
+		}
+		.item-right {
+		  overflow: hidden;
+		  display: flex;
+		  flex-direction: column;
+		  align-items: center;
+			.time {
+			  color: #808080;
+			  font-size: 18upx;
+				height:60upx;
+				line-height: 60upx;
+			}
+			.mark {
+			  background-color: #f74c31;
+			  line-height: 35upx;
+				text-align: center;
+			  font-size: 18upx;
+			  min-width: 35upx;
+			  min-height: 35upx;
+			  border-radius: 18upx;
+			  color: #fff;
+			}
+		}
+	}
+</style>
